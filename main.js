7c622d5fdf24f279d2f4a4563cde3c23ce0a0b63
Nicolas Hatcher <nicoliere@gmail.com>
2017-09-06 21:35:35 +0200

    FIX: First approach to resizing

---
+++ main.js
@@ -0,0 +1,26 @@
+function main(event) {
+    function resize() {
+        var plot_area = document.querySelector('#plot-area');
+        var plot_canvas = document.querySelector('#plot-canvas');
+        plot_canvas.setAttribute('width', plot_area.clientWidth);
+        plot_canvas.setAttribute('height', plot_area.clientHeight);
+        update();
+    }
+    window.addEventListener('resize', () => { resize(); }, true);
+    var editorEl = document.querySelector('#program-area textarea');
+    function update() {
+        plotter('plot-canvas', {
+            f: editor.getValue().trim()
+        });
+    }
+    var editor = CodeMirror.fromTextArea(editorEl, {
+        lineNumbers: true
+    });
+    var program_area = document.querySelector('#program-area');
+    editor.setSize(program_area.clientWidth, program_area.clientHeight);
+    editor.on('change', function() {
+        console.log('change');
+        update();
+    });
+    resize();
+}
